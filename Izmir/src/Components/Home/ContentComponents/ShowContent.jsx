5852ca0d55ba3a70cab16a109e6998a1b6dd9613
Zoha2400 <vasv29834@gmail.com>
2023-08-20 03:12:50 +0500

    Some Changes

--- Izmir/src/Components/Home/ContentComponents/ShowContent.jsx
+++ Izmir/src/Components/Home/ContentComponents/ShowContent.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './ContentComponents.scss'
+import { ReactSVG } from 'react-svg'
+// import {ReactComponent as Logo } from '../../../assets/owSvDveM.svg'
 
 function mainShowContent({data}) {
   return (
     <div className='main-show-content-wrap'>
         <div className='main-show-content'>
            <img src={data.img}/>
+           {/* <ReactSVG src='http://89.38.131.46:1808/static/img/owSvDveM.svg'/> */}
         </div>
     </div>
   )
